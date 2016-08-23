608c764082a2923bba53c1075173da433a23a093
adrian123454321 <adrian.lora12345@gmail.com>
2016-08-22 20:35:20 -0600

    clasesrobot

---
+++ mapas/Programa/clases.py
@@ -0,0 +1,14 @@
+#claserobot
+#comportamiebtos
+	#mover
+	#rotar
+	#recoger
+class robot():
+	def __init__(self,x,y,monedas):
+		self.x=x
+		self.y=y
+	def mover(self):
+
+	def rotar(self):
+
+	def recoger(self):                                            
