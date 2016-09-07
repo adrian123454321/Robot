fa0bf15bcf99c1ddd9440b7ab33a89a747bfb5b5
adrian123454321 <adrian.lora12345@gmail.com>
2016-09-06 20:36:15 -0600

    ROBOT

--- mapas/Programa/mapa.py
+++ mapas/Programa/mapa.py
@@ -6,3 +6,12 @@ class Mapa(object):
 		self.robot=None
 	def cargar_robot(self,robot):
 		self.robot.append(robot)
+
+	def agregar_monedas(self,monedas):
+		self.monedas.append(monedas)
+
+
+	def quitar_monedas(self):
+		for a in (monedas):
+			if a=self.x and a=self.y:
+				self.monedas.pop(monedas)
